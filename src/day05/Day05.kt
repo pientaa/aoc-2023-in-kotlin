a6ff3ec4ee23ffb965202cbfb9f6f8fa2cf6be7e
Łukasz Pięta <pieta.lukasz98@gmail.com>
2023-12-08 00:51:55 +0100

    Day 05

---
+++ src/day05/Day05.kt
@@ -0,0 +1,102 @@
+package day05
+
+import println
+import readText
+
+fun main() {
+
+    fun part1(input: String): Long {
+        val almanacParts: List<String> = input.getAlmanacPartsSplit()
+        val seeds: List<Long> = almanacParts.getSeedsLineSingleNumbers()
+        return getTheLowestLocation(almanacParts, seeds)
+    }
+
+    fun part2(input: String): Long {
+        val almanacParts = input.getAlmanacPartsSplit()
+        val seeds: List<LongRange> = almanacParts.getSeedsLineRangeNumbers()
+        return getTheLowestLocationUsingRanges(almanacParts, seeds)
+    }
+
+    val testInput = readText("Day05_test")
+    val input = readText("Day05")
+
+    println("Part one test:")
+    part1(testInput).println()
+    println("Part one:")
+    part1(input).println()
+    println("Part two test:")
+    part2(testInput).println()
+    println("Part two:")
+    part2(input).println()
+}
+
+private fun getTheLowestLocationUsingRanges(almanacParts: List<String>, seeds: List<LongRange>): Long {
+    val listOfMaps: List<List<Pair<LongRange, Long>>> = almanacParts.getTheListOfMaps()
+
+    return listOfMaps.fold(seeds) { acc: List<LongRange>, map: List<Pair<LongRange, Long>> ->
+        acc.flatMap { inputRange ->
+            val merges: List<LongRange> = map.mapNotNull { (range, diff) ->
+                inputRange.mergeWith(range)?.let { LongRange(it.first + diff, it.last + diff) }
+            }
+
+            if (merges.isNotEmpty()) merges + inputRange.missingRanges(merges)
+            else listOf(inputRange)
+        }
+    }.minOf { it.first }
+}
+
+fun LongRange.mergeWith(other: LongRange): LongRange? {
+    val start = maxOf(this.first, other.first)
+    val end = minOf(this.last, other.last)
+
+    return if (start <= end) start..end else null
+}
+
+fun LongRange.missingRanges(ranges: List<LongRange>): List<LongRange> {
+    val allRanges = mutableListOf<LongRange>()
+    allRanges.add(start..<ranges.first().first)
+
+    for (i in 0 until ranges.size - 1) {
+        val endOfCurrent = ranges[i].last
+        val startOfNext = ranges[i + 1].first
+        if (endOfCurrent < startOfNext - 1) {
+            allRanges.add((endOfCurrent + 1)..<startOfNext)
+        }
+    }
+
+    allRanges.add((ranges.last().last + 1)..endInclusive)
+
+    return allRanges.filter { it.first in this && it.last in this }
+}
+
+private fun getTheLowestLocation(almanacParts: List<String>, seeds: List<Long>): Long {
+    val listOfMaps: List<List<Pair<LongRange, Long>>> = almanacParts.getTheListOfMaps()
+
+    return listOfMaps
+        .fold(seeds) { acc: List<Long>, map: List<Pair<LongRange, Long>> ->
+            acc.map { input -> input + (map.firstOrNull { input in it.first }?.second ?: 0) }
+        }
+        .min()
+}
+
+private fun List<String>.getTheListOfMaps(): List<List<Pair<LongRange, Long>>> =
+    removeSeedsLine()
+        .map { map ->
+            map.splitToLines()
+                .map { mapLine ->
+                    val (destination, source, range) = mapLine.split(" ").map { s -> s.toLong() }
+                    LongRange(source, source + range - 1) to destination - source
+                }
+        }
+
+private fun String.getAlmanacPartsSplit() = split("\n\n")
+private fun List<String>.getSeedsLineSingleNumbers(): List<Long> =
+    first().substringAfter(":").trim().split(" ").map { it.toLong() }
+
+private fun List<String>.getSeedsLineRangeNumbers(): List<LongRange> =
+    getSeedsLineSingleNumbers().windowed(2, 2)
+        .map { LongRange(it.first(), it.first() + it.last() - 1) }
+
+private fun String.splitToLines() = split("\n").drop(1)
+private fun List<String>.removeSeedsLine() = drop(1)
+
